550a8832f9e81cdf4b3787187e157301cdf69175
1sh1vam <shivam07.mt@gmail.com>
2023-11-28 16:18:02 +0530

    feat: player entity type

---
+++ src/types/players.ts
@@ -0,0 +1,18 @@
+export type Player = {
+    id: string;
+    "Player Name": string;
+    "Player Image": string;
+    "Jersey Number": string;
+    "Position": string;
+    "Height": string;
+    "Weight": string;
+    "Nationality": string;
+    "Flag Image": string;
+    "Starter": string;
+    "Appearances": string;
+    "Minutes Played": string;
+    "Goals": string;
+    "Assists": string;
+    "Clean Sheets": string;
+    "Saves": string;
+  }
