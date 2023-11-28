66f4ce8f01fe190fd9976e047f98f2e9b3028ec0
1sh1vam <shivam07.mt@gmail.com>
2023-11-28 16:29:15 +0530

    fix: fixed unnecessary import

--- src/components/inputs/CutomSelect.tsx
+++ src/components/inputs/CutomSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ArroDownIcon from "@/assets/icons/chevron-down.svg?react";
 import { twMerge } from "tailwind-merge";
 
