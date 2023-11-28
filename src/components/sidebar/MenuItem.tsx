c6ec58295d817ab00e8a4923d13438674132a490
1sh1vam <shivam07.mt@gmail.com>
2023-11-28 16:20:27 +0530

    feat: link paths to the menu items in sidebar

--- src/components/sidebar/MenuItem.tsx
+++ src/components/sidebar/MenuItem.tsx
@@ -1,13 +1,16 @@
 import clsx from "clsx";
 import React from "react";
 import { twMerge } from "tailwind-merge";
+import { Link } from 'react-router-dom';
 
 interface MenuItemProps extends React.HTMLAttributes<HTMLDivElement> {
   focused?: boolean;
+  path: string;
 }
 
 const MenuItem = ({
   focused = false,
+  path,
   className,
   children,
   ...props
@@ -22,12 +25,14 @@ const MenuItem = ({
     )
   );
   return (
-    <div {...props} className={containerClass}>
-      {children}
-      {focused ? (
-        <div className="absolute top-1/2 -translate-y-1/2 -left-[5px] w-1 h-1 rounded-full bg-primary-orange" />
-      ) : null}
-    </div>
+    <Link to={path}>
+      <div {...props} className={containerClass}>
+        {children}
+        {focused ? (
+          <div className="absolute top-1/2 -translate-y-1/2 -left-[5px] w-1 h-1 rounded-full bg-primary-orange" />
+        ) : null}
+      </div>
+    </Link>
   );
 };
 
